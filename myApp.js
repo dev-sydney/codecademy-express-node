421504ad98d908185343baf1df91aeb116037048
dev-sydney <sydneyotutey279@gmail.com>
2023-07-06 20:37:23 +0000

    challenage #3 completed

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ let express = require('express');
 let app = express();
 
 console.log('Hello World');
+
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(`${__dirname}/views/index.html`);
 });
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
 module.exports = app;
