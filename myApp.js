25f372d7994d1d0a9b8b8bc80c0833f556c4a298
dev-sydney <sydneyotutey279@gmail.com>
2023-07-06 21:29:35 +0000

    challenage #5 completed

--- myApp.js
+++ myApp.js
@@ -3,9 +3,12 @@ let app = express();
 
 console.log('Hello World');
 
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+// app.use(express.static('public'));
+// app.get('/', (req, res) => {
+//   res.sendFile(`${__dirname}/public/index.html`);
+// });
+app.get('/json', (req, res) => {
+  res.status(200).json({ message: 'Hello there' });
 });
 // app.get('/', (req, res) => {
 //   res.send('Hello Express');
