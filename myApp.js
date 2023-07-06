f728c2fec331dc2e320af9ed47ac2570867d59a3
dev-sydney <sydneyotutey279@gmail.com>
2023-07-06 22:16:17 +0000

    challenage #7 completed

--- myApp.js
+++ myApp.js
@@ -9,13 +9,24 @@ console.log('Hello World');
 // app.get('/', (req, res) => {
 //   res.sendFile(`${__dirname}/public/index.html`);
 // });
-app.get('/json', (req, res) => {
-  let message =
-    process.env.MESSAGE_STYLE === 'uppercase' ? 'HELLO JSON' : 'Hello json';
+// app.get('/json', (req, res) => {
+//   let message =
+//     process.env.MESSAGE_STYLE === 'uppercase' ? 'HELLO JSON' : 'Hello json';
 
-  res.json({ message });
-});
+//   res.json({ message });
+// });
 // app.get('/', (req, res) => {
 //   res.send('Hello Express');
 // });
+
+app.get(
+  '/now',
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 module.exports = app;
